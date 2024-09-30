30fae323d15369921f5214fbee7a9938ed659562
Izzeldeen Ashour <ezraashour00@gmail.com>
2024-09-30 07:40:50 +0300

    push

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,18 +1,11 @@
 import { Metadata } from "next";
 import Wrapper from "@/layouts/wrapper";
 import Header from "@/layouts/header/header";
-import SimpleSlider from "@/components/hero-banner/SimpleSlider";
 import ProductArea from "@/components/product/home/product-area";
-import ProductFeatureArea from "@/components/product/feature/product-feature-area";
-import OfferCountdownBanner from "@/components/product/countdown-banner/offer-countdown-banner";
-import ProductBannerArea from "@/components/banner/product-banner-area";
 import AllProducts from "@/components/product/home/all-products";
 import FeatureArea from "@/components/feature/feature-area";
-import HeroSlider from "@/components/NewSliderForMe/HeroSlider";
 import AboutAreaFour from "@/components/about/about-area-4";
-import WorldWide from "@/components/world-wide-availalbe/worldwide";
 import ContactUs from "@/components/contactUs/ContactUs"
-import CategoryArea from "@/components/category/category-area"
 export const metadata: Metadata = {
   title: "Aqar - Drugs Store"
 };
